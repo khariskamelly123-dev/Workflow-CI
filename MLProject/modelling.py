7be9c2a6a1780b25b1fb83731962b124756a630f
Melly <khariskamelly123@gmail.com>
2026-06-02 23:09:01 +0700

    c

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -21,9 +21,6 @@ X_train, X_test, y_train, y_test = train_test_split(
     random_state=42
 )
 
-# Set experiment
-mlflow.set_experiment("HeartDiseaseExperiment")
-
 # Aktifkan autolog MLflow
 mlflow.sklearn.autolog()
 
